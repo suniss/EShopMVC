f4d9430e9974e97b408059d390b44ef6c1e789e4
sunisakc <sunisa.kan@gmail.com>
2021-08-25 15:07:43 +0200

    Add MockCategory and MockPieRepository

---
+++ EShopMVC/Models/MockCategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShopMVC.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories =>
+            new List<Category>
+            {
+                new Category{CategoryId= 1, CategoryName="Fruit pies", Description="All-FruitPie pie" },
+                new Category{CategoryId= 2, CategoryName="Cheese cake", Description="Cheesy all the way" },
+                new Category{CategoryId= 3, CategoryName="Seasonal Pie", Description="Get in The mood for a seasonal pie" }
+                };
+    }
+}
---
+++ EShopMVC/Models/MockPieRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShopMVC.Models
+{
+    public class MockPieRepository : IPieRepository
+        
+    {
+        private readonly ICategoryRepository _categoryRepository = new MockCategoryRepository();
+
+        public IEnumerable<Pie> AllPies =>
+            new List<Pie>
+            {
+                new Pie {PieId = 1, Name="Strawberry Pie", Price= 15.96M, ShortDescription="Test Description"},
+                new Pie {PieId = 2, Name="Strawberry Pie", Price= 15.96M, ShortDescription="Test Description"},
+                new Pie {PieId = 3, Name="Strawberry Pie", Price= 15.96M, ShortDescription="Test Description"},
+                new Pie {PieId = 4, Name="Strawberry Pie", Price= 15.96M, ShortDescription="Test Description"}
+            };
+public IEnumerable<Pie> PiesOfTheWeek { get; }
+
+        public Pie GetPieById(int pieId)
+        {
+            return AllPies.FirstOrDefault(p => p.PieId == pieId);
+        }
+    }
+}
